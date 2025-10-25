d09c7bffc1fcd82b711eb3d3849507fdc104ca76
ryandobkin <ryandobkin2@gmail.com>
2025-10-24 19:45:26 -0700

    Central icon script to minimize directory targeting
    Not very well implemented and not very well set up (types not set up). Not sure if we'll keep but I think it'd be a useful thing to have.

---
+++ frontend/src/assets/icons.tsx
@@ -0,0 +1,16 @@
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+
+export const Icons = {
+    osu_logo: {
+        light:  "../images/osu-horizontal-light.svg",
+        dark:   "../images/osu-horizontal-dark.svg",
+    },
+    github: {
+        light:  "../images/github-mark-light.svg",
+        dark:   "../images/osu-horizontal-dark.svg",
+    },
+    themeToggle: {
+        light:  <SunIcon />,
+        dark:   <MoonIcon />,
+    },
+};
