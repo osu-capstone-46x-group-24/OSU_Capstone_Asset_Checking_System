596d3b3117aaa2e23f97e670a590da6e70e99a2a
ryandobkin <ryandobkin2@gmail.com>
2025-10-24 19:43:38 -0700

    Basic header component
    Should be updated to be extensible and should take buttons / icons to be displayed as inputs and display them arbitrarily.

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,23 @@
+//import ButtonIcon from "./ButtonIcon.tsx";
+import { MoonIcon } from "@heroicons/react/24/solid";
+import osu_horizontal_light from "../images/osu-horizontal-light.svg";
+import "tailwindcss";
+
+function Header() {
+    return (
+        <div className="absolute flex flex-row justify-self-left inset-x-0 top-0 w-full bg-white h-1/10 items-center pr-3 pl-3">
+
+            <a href="/" className="basis-10/100 self-center align-flex">
+                <img src={osu_horizontal_light} alt="osu logo / home button"/>
+            </a>
+
+            <div className="grow"></div>
+
+            <a href="#" className="basis-16 self-center content-end text-gray-800">
+                <MoonIcon className="color-white w-16 h-16"/>
+            </a>
+        </div>
+    );
+}
+
+export default Header;
