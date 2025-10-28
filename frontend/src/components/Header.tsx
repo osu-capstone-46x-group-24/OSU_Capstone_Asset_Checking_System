952446cfebce52977b506adb63020d197ca7f96a
ryandobkin <ryandobkin2@gmail.com>
2025-10-28 15:44:16 -0700

    Standardized light/dark theming

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,8 @@ import "tailwindcss";
 
 function Header() {
     return (
-        <div className="absolute flex flex-row justify-self-left inset-x-0 top-0 w-full bg-white h-1/10 items-center pr-3 pl-3">
+        <div className="absolute flex flex-row justify-self-left inset-x-0 top-0 w-full h-1/10 items-center pr-3 pl-3
+        bg-gray-800 dark:bg-gray-200 font-sans font-size-body">
 
             <a href="/" className="basis-10/100 self-center align-flex">
                 <img src={osu_horizontal_light} alt="osu logo / home button"/>
