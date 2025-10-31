fffecb1e56d628f1b236faabf48d2a3968ceadaa
EthanShiota <54644546+EthanShiota@users.noreply.github.com>
2025-10-31 11:50:28 -0700

    inital drizzle commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
+
+const db = drizzle(process.env.DB_FILE_NAME!);
 const app = new Hono();
 
 app.get("/", (c) => {
