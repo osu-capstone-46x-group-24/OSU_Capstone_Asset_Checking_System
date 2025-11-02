07f6109bdac58b738ae371b25f7fc46bcfa37540
ryandobkin <ryandobkin2@gmail.com>
2025-11-02 14:54:37 -0800

    Standardized Page implementation
    Changed page layout to be split between /Home, /User, and /Admin. The '/' endpoint will resolve to /Home. Moved the content body from a dedicated component into the page component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,51 @@
-import './App.css'
-import SplashPage from "./Pages/SplashPage.tsx";
+//import React from "react";
+import { useEffect} from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserDashboard from "./pages/UserDashboard";
+
+//const ThemeContext = createContext(null);
 
 function App() {
+    useEffect(() => {
+        // Use the user selected theme if chosen
+        //const [theme, setTheme] = useState('light');
+        const selectedTheme = localStorage.getItem("theme");
 
-  return (
-    <div className="bg-amber-100 dark:bg-gray-950">
-        <SplashPage />
-    </div>
+        if (selectedTheme) {
+            document.body.classList.add(selectedTheme);
+        }
+
+        // If user OS set to prefer dark mode
+        else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            document.body.classList.add("dark");
+        }
+
+        // else fallback to light mode
+        else {
+            document.body.classList.add("light");
+        }
+    }, []);
+
+    return (
+          <Router>
+              <Routes>
+                  <Route path="/" element={<Navigate to="/Home" />} />
+                  <Route path="/Home" element={<Home />} />
+                  {/*<Route path="/about" element={<About />} />*/}
+                  <Route path="/Admin" element={<AdminDashboard />} />
+                  <Route path="/User" element={<UserDashboard />} />
+              </Routes>
+          </Router>
+
+
   );
 }
 
---
+++ frontend/src/pages/UserDashboard.tsx
@@ -0,0 +1,30 @@
+import '../App.css'
+import Footer from "../components/UI_Elements/Footer.tsx";
+import Navbar from "../components/UI_Elements/Navbar.tsx";
+
+function UserDashboard() {
+
+    return (
+        <div className="absolute w-full top-0 left-0 bg-neutral-100 dark:bg-neutral-800">
+
+            <Navbar />
+
+            {/* Body */}
+            <div className="relative min-h-180 w-full pt-19">
+                <div className="flex flex-col pt-20">
+                    <div className="basis-12 py-6 text-7xl font-sans font-size-body mt-20">
+                        <a
+                            href="/Home">
+                            User Dashboard
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <Footer/>
+
+        </div>
+    );
+}
+
+export default UserDashboard;
