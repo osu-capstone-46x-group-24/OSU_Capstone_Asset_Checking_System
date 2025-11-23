9c14cfe83ec4db73d3695a2c62099a6befd58c53
ryandobkin <ryandobkin2@gmail.com>
2025-11-23 15:53:42 -0800

    Revised to use base button component, correctly take props, removed MimeScanButton (deprecated)

--- frontend/src/components/UI_Elements/ButtonDefault.tsx
+++ frontend/src/components/UI_Elements/ButtonDefault.tsx
@@ -1,12 +1,31 @@
 // ButtonDefault.tsx
 
-import { Button } from "@material-tailwind/react";
 import type { ButtonProps } from "@material-tailwind/react";
 
-export default function ButtonDefault(props: ButtonProps) {
+/**
+ * ButtonDefault Component
+ * @param props
+ * @constructor
+ */
+export default function ButtonDefault({
+    props,
+    onClick,
+    children,
+    className,
+}: {
+    props?: ButtonProps;
+    onClick?: () => void;
+    children?: string;
+    className?: string;
+}) {
     return (
-        <div className="flex items-center gap-4">
-            <Button {...props} />
+        <div className={`flex items-center gap-4`}>
+            <button
+                {...props}
+                onClick={onClick}
+                children={children}
+                className={className}
+            />
         </div>
     );
 }
