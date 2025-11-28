509c5de85c92f937baf99901ac2e9f66f5c7c6d4
ryandobkin <ryandobkin2@gmail.com>
2025-11-27 20:10:26 -0800

    Added updated theme functionality, added RFID console component to admin dashboard. *WIP but functional*

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,7 +1,18 @@
+// AdminDashboard.tsx
+
 import "../App.css";
 import Footer from "../components/UI_Elements/Footer.tsx";
 import Navbar from "../components/UI_Elements/Navbar.tsx";
+import LogRFIDRequests from "../components/LogRFIDRequests";
 
+
+/**
+ * AdminDashboard
+ * @type page
+ * @param theme
+ * @param setTheme
+ * @constructor
+ */
 function AdminDashboard({
     theme,
     setTheme,
@@ -9,23 +20,34 @@ function AdminDashboard({
     theme: "light" | "dark";
     setTheme: (t: "light" | "dark") => void;
 }) {
+
+    const pageTheme =
+        theme === "light"
+            ? "bg-wu-gray-200 text-wu-gray-400"
+            : "bg-wu-gray-400 text-wu-gray-200"
+
+    const consoleTheme =
+        theme === "light"
+            ? "bg-wu-gray-400 text-wu-gray-200"
+            : "bg-wu-gray-500 text-wu-gray-200";
+
     return (
         <div
             className={`absolute w-full top-0 left-0 font-mono duration-300 transition-colors
-        ${
-            theme === "light"
-                ? "bg-wu-gray-200 text-wu-gray-400"
-                : "bg-wu-gray-400 text-wu-gray-200"
-        }`}
+            ${pageTheme}`}
         >
             <Navbar theme={theme} setTheme={setTheme} />
 
             {/* Body */}
-            <div className="relative min-h-180 w-full pt-19">
-                <div className="flex flex-col pt-20">
-                    <div className="basis-12 py-6 text-7xl font-size-body mt-20">
-                        <a href="#">Admin Dashboard</a>
-                    </div>
+            <div className="justify-items-start w-full pb-1 pt-10 text-5xl">
+                <span className="">Admin Dashboard</span>
+            </div>
+            <div className="min-h-180 max-w-8/10 w-full p-10 flex flex-col">
+                <div className="flex p-0.5 text-xl">
+                    <span>RFID Request Console</span>
+                </div>
+                <div className={`h-140 items-top p-1 w-full rounded-sm shadow-lg self-start justify-self-stretch ${consoleTheme}`}>
+                        <LogRFIDRequests />
                 </div>
             </div>
 
