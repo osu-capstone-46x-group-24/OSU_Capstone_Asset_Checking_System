a181c007f9f37a7ab689ced82a62deba6f3761f9
ryandobkin <ryandobkin2@gmail.com>
2025-11-27 20:07:58 -0800

    Changed constants to use config file for rfid server port, now correctly listens for and handles incoming rfid requests

--- frontend/src/hooks/UseSocket.tsx
+++ frontend/src/hooks/UseSocket.tsx
@@ -2,17 +2,59 @@
 
 // Imports
 import { io } from "socket.io-client";
+import configJson from "../spec_config.json";
 
 // Constants
-const PORT = 3000;
-const BASE_URL = `http://localhost:${PORT}`;
+const PORT: number = configJson.scanner_config.port;
+const BASE_URL: string = `http://localhost:${PORT}`;
 const socket = io(BASE_URL);
+
+type ReqItem = {
+    reqType: string;
+    itemName: string;
+    timestamp: string;
+    raw: string;
+};
 
 /**
  * useSocket Hook
+ * Listens for socket events from *scanner* server
  */
-export default function useSocket() {
-    socket.on("card", (cardId) => {
-        console.log("Received card event: ", cardId);
+export default function useSocket({ onRequest }: { onRequest: any }) {
+    function sendRequestToParent(requestType: string, message: string) {
+        const newRequest: ReqItem = {
+            reqType: requestType,
+            itemName: message,
+            timestamp: getTimeStamp(),
+            raw: message,
+        }
+        onRequest(newRequest);
+    }
+
+    function getTimeStamp(): string {
+        let epochTime = Date.now();
+        let formattedTime = new Date(epochTime).toString();
+        return formattedTime;
+    }
+
+    // listen for socket events
+    socket.on("message", (msg: string): void => {
+        console.log(`Message from server: ${msg}`);
+        sendRequestToParent("message", msg);
     });
+
+    socket.on("card", (cardId: string): void => {
+        console.log(`Card scanned: ${cardId}`);
+        sendRequestToParent("card", cardId);
+    });
+
+    socket.on("item", (itemId: string): void => {
+        console.log(`Item scanned: ${itemId}`);
+        sendRequestToParent("item", itemId);
+    });
+
+    return (
+        <>
+        </>
+    );
 }
