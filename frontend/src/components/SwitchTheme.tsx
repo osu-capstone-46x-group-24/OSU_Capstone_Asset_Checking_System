e1d8ae0ee5ee37298c533e2f079897e10c437c78
ryandobkin <ryandobkin2@gmail.com>
2025-11-27 20:05:55 -0800

    Fixed theme slider starting .25px too far right

--- frontend/src/components/SwitchTheme.tsx
+++ frontend/src/components/SwitchTheme.tsx
@@ -28,9 +28,9 @@ export default function SwitchTheme({
             />
             <label
                 htmlFor="switch-component-dark"
-                className="absolute top-0.25 left-0.5 w-5 h-5 bg-wu-gray-200 peer-checked:bg-wu-gray-500
+                className="absolute top-0.25 left-0.25 w-5 h-5 bg-wu-gray-200 peer-checked:bg-wu-gray-500
                 peer-checked:hover:border-wu-gray-200 hover:border-wu-gray-500 rounded-full border border-wu-gray-200 shadow-sm
-                 transition-all duration-300 peer-checked:translate-x-4.25 peer-checked:border-wu-gray-500 cursor-pointer"
+                 transition-all duration-300 peer-checked:translate-x-4.5 peer-checked:border-wu-gray-500 cursor-pointer"
             ></label>
         </div>
     );
