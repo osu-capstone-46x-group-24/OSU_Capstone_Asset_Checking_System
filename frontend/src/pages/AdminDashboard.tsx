f12ef201e3a5d6e164ad79a02bf70916fe0d70b1
ryandobkin <ryandobkin2@gmail.com>
2025-11-29 19:12:18 -0800

    Pass theme variables as props when not passing through config file, changed implementation to be more reliant on LogRFIDRequests component rather than adminDashboard css

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -26,10 +26,10 @@ function AdminDashboard({
             ? "bg-wu-gray-200 text-wu-gray-400"
             : "bg-wu-gray-400 text-wu-gray-200"
 
-    const consoleTheme =
+    const consoleParams =
         theme === "light"
-            ? "bg-wu-gray-400 text-wu-gray-200"
-            : "bg-wu-gray-500 text-wu-gray-200";
+            ? {color_primary_text: "wu-gray-200", color_primary_bg: "wu-gray-400", color_accent_text: "wu-gray-400", color_accent_bg: "wu-gray-200"}
+            : {color_primary_text: "wu-gray-200", color_primary_bg: "wu-gray-500", color_accent_text: "wu-gray-500", color_accent_bg: "wu-gray-200"};
 
     return (
         <div
@@ -39,18 +39,21 @@ function AdminDashboard({
             <Navbar theme={theme} setTheme={setTheme} />
 
             {/* Body */}
-            <div className="justify-items-start w-full pb-1 pt-10 text-5xl">
-                <span className="">Admin Dashboard</span>
-            </div>
-            <div className="min-h-180 max-w-8/10 w-full p-10 flex flex-col">
-                <div className="flex p-0.5 text-xl">
-                    <span>RFID Request Console</span>
+            <div className="flex-grow">
+                <div className="justify-items-start w-full pb-1 pt-10 text-5xl">
+                    <span className="">Admin Dashboard</span>
                 </div>
-                <div className={`h-140 items-top p-1 w-full rounded-sm shadow-lg self-start justify-self-stretch ${consoleTheme}`}>
-                        <LogRFIDRequests />
+                <div className="min-h-[600px] min-w-[1000px] w-full p-10 flex flex-col">
+                    <div className="flex p-0.5 text-xl">
+                        <span>RFID Request Console</span>
+                    </div>
+                    <div className={`p-1`}>
+                            <LogRFIDRequests
+                                {...consoleParams}
+                            />
+                    </div>
                 </div>
             </div>
-
             <Footer theme={theme} />
         </div>
     );
