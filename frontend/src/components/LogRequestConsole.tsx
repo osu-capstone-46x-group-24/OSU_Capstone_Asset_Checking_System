c4367f69a2a575f24f7b70c5a10d2262c92ca05e
ryandobkin <ryandobkin2@gmail.com>
2025-12-01 17:55:54 -0800

    Reformat

--- frontend/src/components/LogRequestConsole.tsx
+++ frontend/src/components/LogRequestConsole.tsx
@@ -1,7 +1,7 @@
 // LogRequestConsole.tsx
 
 // Imports
-import {useCallback, useEffect, useRef, useState} from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import configJson from "../spec_config.json";
 import UseSocket from "../hooks/UseSocket";
 
@@ -16,43 +16,43 @@ type ConsoleItem = {
 
 type LogRequestConsoleProps = {
     color_primary_text: string;
-    color_primary_bg: string,
-    color_accent_text: string,
-    color_accent_bg: string
-}
+    color_primary_bg: string;
+    color_accent_text: string;
+    color_accent_bg: string;
+};
 
 // Constants
 const color_placeholder_text = "wu-gray-300";
 
 /**
- * Name: LogRequestConsole \
- * Type: Component \
- * Description: Takes styling properties as props and displays a console that displays incoming requests caught via the UseSocket.tsx hook \
- * Props: color_primary_text, color_primary_bg, color_accent_text, color_accent_bg \
+ * Name: LogRequestConsole
+ * Type: Component
+ * Description: Takes styling properties as props and displays a console that displays incoming requests caught via the UseSocket.tsx hook
+ * Props: color_primary_text, color_primary_bg, color_accent_text, color_accent_bg
  */
 export default function LogRequestConsole({
     color_primary_text,
     color_primary_bg,
     color_accent_text,
     color_accent_bg,
-}: LogRequestConsoleProps ){
+}: LogRequestConsoleProps) {
     const [consoleItems, setConsoleItems] = useState<ConsoleItem[]>([]);
     const containerRef = useRef<HTMLDivElement | null>(null);
 
     const addNetworkTransactionLog = useCallback((newLog: ConsoleItem) => {
-        setConsoleItems(prev => [...prev, newLog]);
+        setConsoleItems((prev) => [...prev, newLog]);
     }, []);
 
     // Color-code message types *based on spec_config.json file
     function getReqTypeColor(reqType: string) {
         if (reqType == "message") {
-            return (configJson.theme.color.request.message);
+            return configJson.theme.color.request.message;
         } else if (reqType == "item") {
-            return (configJson.theme.color.request.item);
+            return configJson.theme.color.request.item;
         } else if (reqType == "card") {
-            return (configJson.theme.color.request.card);
+            return configJson.theme.color.request.card;
         } else {
-            return ("text-wu-gray-200");
+            return "text-wu-gray-200";
         }
     }
 
@@ -77,17 +77,25 @@ export default function LogRequestConsole({
                 ref={containerRef}
             >
                 <table className="divide-y mb-6 m-1 text-left">
-                    <thead className={`sticky top-1 text-center p-1 text-${color_accent_text}`}>
+                    <thead
+                        className={`sticky top-1 text-center p-1 text-${color_accent_text}`}
+                    >
                         <tr className={`w-min bg-${color_accent_bg}`}>
-                            <th className="px-4 whitespace-nowrap">Timestamp</th>
+                            <th className="px-4 whitespace-nowrap">
+                                Timestamp
+                            </th>
                             <th className="px-4 whitespace-nowrap">Sender</th>
-                            <th className="px-4 whitespace-nowrap">Request Type</th>
+                            <th className="px-4 whitespace-nowrap">
+                                Request Type
+                            </th>
                             <th className="px-4 whitespace-nowrap">RAW</th>
                             <th className="px-4 whitespace-nowrap">Message</th>
                             <th className="w-full"></th>
                         </tr>
                     </thead>
-                    <tbody className={`pt-[32px] min-w-full text-${color_primary_text}`}>
+                    <tbody
+                        className={`pt-[32px] min-w-full text-${color_primary_text}`}
+                    >
                         {consoleItems.length === 0 ? (
                             <tr>
                                 <td
@@ -104,28 +112,33 @@ export default function LogRequestConsole({
                                         key={index}
                                         className={`border-b text-center border-dashed border-${color_primary_text} nth-[2n]:border-wu-gray-300 text-xs uppercase`}
                                     >
-                                        <td className="px-4 py-2 whitespace-nowrap"
+                                        <td
+                                            className="px-4 py-2 whitespace-nowrap"
                                             colSpan={1}
                                         >
                                             [{item.timestamp}]
                                         </td>
-                                        <td className="px-4 py-2 whitespace-nowrap"
+                                        <td
+                                            className="px-4 py-2 whitespace-nowrap"
                                             colSpan={1}
                                         >
                                             {item.sender}
                                         </td>
-                                        <td className={`px-4 py-2 whitespace-nowrap 
+                                        <td
+                                            className={`px-4 py-2 whitespace-nowrap 
                                             ${getReqTypeColor(item.reqType)}`}
                                             colSpan={1}
                                         >
                                             {item.reqType}
                                         </td>
-                                        <td className="px-4 py-2 whitespace-nowrap"
+                                        <td
+                                            className="px-4 py-2 whitespace-nowrap"
                                             colSpan={1}
                                         >
                                             {item.raw}
                                         </td>
-                                        <td className="px-4 py-2 whitespace-nowrap"
+                                        <td
+                                            className="px-4 py-2 whitespace-nowrap"
                                             colSpan={1}
                                         >
                                             {item.itemName}
@@ -134,7 +147,6 @@ export default function LogRequestConsole({
                                     </tr>
                                 )
                             )
-
                         )}
                     </tbody>
                 </table>
--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -7,16 +7,19 @@ import Navbar from "../components/UI_Elements/Navbar.tsx";
 import LogRequestConsole from "../components/LogRequestConsole";
 
 // Type
-type FilterProperty = {
-    name: string,
-    boolValue?: boolean,
-    strValue?: string,
-}
+type AdminDashboardProps = {
+    theme: "light" | "dark";
+    setTheme: (t: "light" | "dark") => void;
+};
+/* type FilterProperty = {
+    name: string;
+    boolValue?: boolean;
+    strValue?: string;
+};
 
 // Const
-const filterList = {
-
-}
+const filterList = {};
+ */
 
 /**
  * AdminDashboard
@@ -26,20 +29,26 @@ const filterList = {
 export default function AdminDashboard({
     theme,
     setTheme,
-}: {
-    theme: "light" | "dark";
-    setTheme: (t: "light" | "dark") => void;
-}) {
-
+}: AdminDashboardProps) {
     const pageTheme =
         theme === "light"
             ? "bg-wu-gray-200 text-wu-gray-400"
-            : "bg-wu-gray-400 text-wu-gray-200"
+            : "bg-wu-gray-400 text-wu-gray-200";
 
     const consoleParams =
         theme === "light"
-            ? {color_primary_text: "wu-gray-200", color_primary_bg: "wu-gray-400", color_accent_text: "wu-gray-400", color_accent_bg: "wu-gray-200"}
-            : {color_primary_text: "wu-gray-200", color_primary_bg: "wu-gray-500", color_accent_text: "wu-gray-500", color_accent_bg: "wu-gray-200"};
+            ? {
+                  color_primary_text: "wu-gray-200",
+                  color_primary_bg: "wu-gray-400",
+                  color_accent_text: "wu-gray-400",
+                  color_accent_bg: "wu-gray-200",
+              }
+            : {
+                  color_primary_text: "wu-gray-200",
+                  color_primary_bg: "wu-gray-500",
+                  color_accent_text: "wu-gray-500",
+                  color_accent_bg: "wu-gray-200",
+              };
 
     return (
         <div
@@ -53,21 +62,16 @@ export default function AdminDashboard({
                 <div className="justify-items-start w-full pb-1 pt-10 text-5xl">
                     <span className="">Admin Dashboard</span>
                 </div>
-                <div className={`w-[100px] h-[600px] flex flex-col ${pageTheme}`}>
-
-                </div>
                 <div className="min-h-[600px] min-w-[1000px] w-full p-10 flex flex-col">
                     <div className="flex p-0.5 text-xl">
                         <span>Request Console</span>
                     </div>
                     <div className={`p-1`}>
-                            <LogRequestConsole
-                                {...consoleParams}
-                            />
+                        <LogRequestConsole {...consoleParams} />
                     </div>
                 </div>
             </div>
             <Footer theme={theme} />
         </div>
     );
-};
+}
