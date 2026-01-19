aa67331d66c292e23241aa5c2d4e0744f6f579d3
ryandobkin <ryandobkin2@gmail.com>
2026-01-18 16:21:16 -0800

    Added NetworkManager implementation, formally added stateful logic passing between child components, controlled in AdminDashboard

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -5,21 +5,24 @@ import "../App.css";
 import Footer from "../components/UI_Elements/Footer.tsx";
 import Navbar from "../components/UI_Elements/Navbar.tsx";
 import LogRequestConsole from "../components/LogRequestConsole";
+import NetworkManager from "../API/NetworkManager.tsx";
+import { useState } from "react";
 
 // Type
 type AdminDashboardProps = {
     theme: "light" | "dark";
     setTheme: (t: "light" | "dark") => void;
 };
-/* type FilterProperty = {
-    name: string;
-    boolValue?: boolean;
-    strValue?: string;
-};
 
-// Const
-const filterList = {};
- */
+type NetworkLogItem = {
+    reqType: string;
+    sender: string;
+    destination: string;
+    itemName: string;
+    timestamp: string;
+    httpType: string;
+    endpoint: string;
+};
 
 /**
  * AdminDashboard
@@ -30,6 +33,13 @@ export default function AdminDashboard({
     theme,
     setTheme,
 }: AdminDashboardProps) {
+    // networkLogs State
+    const [networkLogs, setNetworkLogs] = useState<NetworkLogItem[]>([]);
+    const addNetworkLog = (newLog: NetworkLogItem) => {
+        setNetworkLogs((prev) => [...prev, newLog]);
+    };
+
+    // Theme
     const pageTheme =
         theme === "light"
             ? "bg-wu-gray-200 text-wu-gray-400"
@@ -62,12 +72,28 @@ export default function AdminDashboard({
                 <div className="justify-items-start w-full pb-1 pt-10 text-5xl">
                     <span className="">Admin Dashboard</span>
                 </div>
-                <div className="min-h-[600px] min-w-[1000px] w-full p-10 flex flex-col">
-                    <div className="flex p-0.5 text-xl">
-                        <span>Request Console</span>
+                <div className="flex flex-row">
+                    <div className="min-h-[600px] min-w-[1000px] w-7/10 p-10 flex flex-col">
+                        <div className="flex p-0.5 text-xl">
+                            <span>Request Console</span>
+                        </div>
+                        <div className={`p-1`}>
+                            <LogRequestConsole
+                                {...consoleParams}
+                                networkLogItems={networkLogs}
+                            />
+                        </div>
                     </div>
-                    <div className={`p-1`}>
-                        <LogRequestConsole {...consoleParams} />
+                    <div className="min-h-[150px] min-w-[100px] p-10 flex flex-col">
+                        <div className="flex p-0.5 text-xl">
+                            <span>Send API Request</span>
+                        </div>
+                        <div className={`p-1`}>
+                            <NetworkManager
+                                {...consoleParams}
+                                onRequest={addNetworkLog}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
