700d3c5b4593ef669b0d080ce039afe3e8729beb
ryandobkin <ryandobkin2@gmail.com>
2026-05-21 22:59:21 -0700

    Updated ScannerFlow -- Now has system in place to check in items
    Upon scanning an ITEM while on the ScannerFlow page, the item will GET the database of items, check the RFID of the scanned ITEM to the items in the list. If it finds an entry with a matching RFID, it will send a POST to /checkin with that entry's ID. Does not currently have visual confirmation or visual error alerts.

--- frontend/src/pages/ScannerFlow.tsx
+++ frontend/src/pages/ScannerFlow.tsx
@@ -2,13 +2,17 @@ import type { ReqItem } from "../../../.d.ts";
 import { useSocket } from "../hooks/useSocket.tsx";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { sendPostRequest } from "../API/OutboundNetworkHandler.ts";
+import {
+    sendGetRequest,
+    sendPostRequest,
+} from "../API/OutboundNetworkHandler.ts";
 
 export default function ScannerFlow() {
     const navigate = useNavigate();
     const [navigating, setNavigating] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
     const [userId, setUserId] = useState<string>("");
+    const [itemChecked, setItemChecked] = useState<boolean>(false);
 
     const handleScan = async (req: ReqItem) => {
         // Only react to card scans
@@ -29,10 +33,62 @@ export default function ScannerFlow() {
                     navigate("/User?id=" + scannedUserId);
                 }, 3000);
             }
+        } else if (req.reqType === "ITEM") {
+            try {
+                console.log("ATTEMPT CHECK IN: ", req.itemName);
+                // Get Database of items
+                // eg
+                // [{"id":1,"name":"TEST","rfid":"001"},
+                // {"id":2,"name":"TEST2","rfid":"002"},
+                // {"id":3,"name":"TEST ELEMENT 3","rfid":"HALLO"},
+                // {"id":4,"name":"Test element 4","rfid":"909992392939"},
+                // {"id":5,"name":"","rfid":""},
+                // {"id":6,"name":"TestElement","rfid":"20302323"},
+                // {"id":7,"name":"sdds","rfid":"dsds"},
+                // {"id":8,"name":"Admin Item","rfid":"0021"},
+                // {"id":10,"name":"EKG","rfid":"00:80:61:3e:89:36:75:04"}]
+                const databaseItemList = await sendGetRequest("/items/all");
+                console.log("DATABASE ITEM LIST:\n", databaseItemList);
+                // Get item entry: example [{"id":2,"name":"TEST2","rfid":"002"}]
+                const itemEntry = databaseItemList.find(
+                    (item: any) => item.rfid === req.itemName
+                );
+                console.log(itemEntry);
+                // Pass id, name, and rfid to itemCheckinFlow()
+                await sendPostRequest("/checkin", {
+                    itemId: itemEntry.id,
+                });
+                itemCheckinFlow(itemEntry);
+            } catch (error) {
+                console.error(error);
+            }
         }
     };
 
     useSocket(handleScan);
+
+    function itemCheckinFlow(itemEntry: any) {
+        setItemChecked(true);
+        if (itemChecked) {
+            return (
+                <div
+                    className={`w-full min-h-screen bg-bg text-text flex items-center justify-center`}
+                >
+                    <h1 className={`text-3xl font-bold mb-20`}>
+                        Item Scanned!
+                    </h1>
+                    <h2 className={`text-xl font-bold mt-20`}>
+                        Checked in: {itemEntry.name} {`\n`}
+                        Item ID: {itemEntry.id} {`\n`}
+                        RFID: {itemEntry.rfid}
+                    </h2>
+                </div>
+            );
+        }
+        setTimeout(() => {
+            setItemChecked(false);
+        }, 3000);
+    }
 
     if (navigating) {
         return (
